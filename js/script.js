a238319ca7de8088c8c703cc94a4025b3e4a2b53
Ben Stewart <sbenstewart@gmail.com>
2018-09-03 11:31:44 +0530

    Created new branch

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 jQuery(function($){
     $('body').on('mouseenter', 'a', function(e) {
         var o = this;
+	let url = this.url;
         if ( o.href != '#' ) {
             chrome.extension.sendRequest('show', function(r) {
                 var uri = $.url.parse(o.href),
@@ -25,15 +26,33 @@ jQuery(function($){
                             y: ( r.position === 'left bottom' ? -20 : 0 )
                         }
                     }
-                }
+                } 
                 // Is the target a new window?
                 if ( $(o).attr('target') == '_blank' ) text = '<i class="fa fa-external-link-square" style="padding-right: 5px;"></i>' + text;
                 // Show the qtip
                 $(o).qtip({
                     overwrite: false,
-                    content: {
-                        text: text
-                    },
+		    content: 
+			{
+text: function(event, api) {
+                    $.ajax({
+			type: "GET",
+			crossDomain: true,
+                        url: api.elements.target.attr('o.href') // Use href attribute as URL
+                    })
+                    .then(function(content) {
+                        // Set the tooltip content upon successful retrieval
+                        api.set('content.text', content);
+                    }, function(xhr, status, error) {
+                        // Upon failure... set the tooltip content to error
+                        api.set('content.text', status + ': ' + error);
+                    });
+        
+                    return 'Loading...'; // Set some initial text
+                }
+
+					}
+                    ,
                     show: {
                         event: e.type,
                         ready: true,
